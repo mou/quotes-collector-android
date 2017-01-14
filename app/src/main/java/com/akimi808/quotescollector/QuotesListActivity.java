a4b275a3d2db2bdbded8ba1fff0128c9dd440cc8
akimi808 <doraneko085@gmail.com>
2017-01-14 21:59:04 +0300

    1. Implemented quotes and books download.

--- app/src/main/java/com/akimi808/quotescollector/QuotesListActivity.java
+++ app/src/main/java/com/akimi808/quotescollector/QuotesListActivity.java
@@ -1,5 +1,6 @@
 package com.akimi808.quotescollector;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +11,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.akimi808.bookmateclient.Bookmate;
+import com.akimi808.bookmateclient.model.Book;
+import com.akimi808.bookmateclient.model.Quote;
 import com.akimi808.quotescollector.db.DbQuoteManager;
 import com.akimi808.quotescollector.db.QuoteDbOpenHelper;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class QuotesListActivity extends AppCompatActivity {
 
@@ -48,10 +62,74 @@ public class QuotesListActivity extends AppCompatActivity {
     }
 
     class SynchronizeTask extends AsyncTask<Void, Void, String> {
+        private final ProgressDialog dialog = new ProgressDialog(QuotesListActivity.this);
+
         @Override
         protected String doInBackground(Void... voids) {
+            Bookmate bookmate = new Retrofit.Builder()
+                    .baseUrl("http://bookmate.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(Bookmate.class);
 
+            downloadQuotes(bookmate);
             return "";
+        }
+
+        private void downloadQuotes(Bookmate bookmate) {
+            int quoteListSize = 0;
+            int page = 1;
+            int quoteCount = 0;
+            Set<String> downloadedBooks = new HashSet<>();
+            do {
+                //возвращает promise, что внутри объекта появится список с цитатами, обещание сделать работу
+                Call<List<Quote>> quotes = bookmate.quotes("vkontakte085", false, page, 10);
+                try {
+                    //результат выполнения работы, ответ сервера, там м.б. ошибка
+                    Response<List<Quote>> response = quotes.execute();
+                    List<Quote> quoteList = response.body();
+                    quoteListSize = quoteList.size();
+                    for (Quote quote : quoteList) {
+                        Log.d("Sync", quote.toString());
+                        String documentUuid = quote.getDocumentUuid();
+                        if (!downloadedBooks.contains(documentUuid)) {
+                            downloadBook(bookmate, documentUuid);
+                            downloadedBooks.add(documentUuid);
+                        } else {
+                            Log.d("Sync", "Book with uuid [" + documentUuid + "] is already downloaded");
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                page = page + 1;
+                quoteCount = quoteCount + quoteListSize;
+            } while (quoteListSize == 10);
+            Log.d("Sync", "Total Quotes downloaded: " + quoteCount);
+        }
+
+        private void downloadBook(Bookmate bookmate, String documentUuid) {
+            Call<Book> bookCall = bookmate.book(documentUuid);
+            try {
+                Response<Book> response = bookCall.execute();
+                Book book = response.body();
+                Log.d("Sync", book.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Synchronize with Bookmate...");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            dialog.dismiss();
+            super.onPostExecute(s);
         }
     }
 
